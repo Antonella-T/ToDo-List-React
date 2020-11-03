468a392b91a2ee2b40424478d44d0d1cfdfa1f8a
Antonella Testa <antonellamtesta@gmail.com>
2020-11-03 13:12:46 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -33,16 +33,18 @@ class App extends React.Component {
 
   addItem() {
     const itemState = this.state.itemToDo;
-    itemState.push({
-      description: this.state.newItem,
-      checked: false
-    });
-
-    this.setState({
-      newItem: "",
-      itemToDo: itemState
-    });
-    this.updateLocalStorage();
+    if(this.state.newItem) {
+      itemState.push({
+        description: this.state.newItem,
+        checked: false
+      });
+  
+      this.setState({
+        newItem: "",
+        itemToDo: itemState
+      });
+      this.updateLocalStorage();
+    }
   };
 
   toggleCheckbox(index) {
@@ -57,17 +59,19 @@ class App extends React.Component {
   render() {
     return (
       <div id="todo-app-container">
-        <h1>To Do App</h1>
-        <div className="add-item">
-          <input type="text" 
-                 placeholder="Type something..."
-                 value={this.state.newItem} 
-                 onChange={(e) => {
-                   this.setState({newItem:e.target.value});
-                 }}>
-          </input>
-          <button onClick={() => this.addItem()}>ADD</button>
-        </div>
+        <header>
+          <h1>To Do App</h1>
+          <div className="add-item">
+            <input type="text" 
+                  placeholder="Type something..."
+                  value={this.state.newItem} 
+                  onChange={(e) => {
+                    this.setState({newItem:e.target.value});
+                  }}>
+            </input>
+            <button onClick={() => this.addItem()}>ADD</button>
+          </div>
+        </header>
         
         {this.state.itemToDo.map((item, index) => {
           return <div key={index} className="item">
